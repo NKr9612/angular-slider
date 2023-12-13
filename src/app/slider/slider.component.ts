a91193ce56ee231c6992d6526b61c0f7985461a5
Norbert Krupa <toftnqr@gmail.com>
2023-12-13 21:44:13 +0100

    clean

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -8,36 +8,16 @@ import { Component, Input } from '@angular/core';
 export class SliderComponent {
   @Input() items: any[] = []; // Dane otrzymane z backendu
   activeIndex: number = 0;
-  prevIndex: number = this.items.length - 1;
-  nextIndex: number = 1;
 
   setActive(index: number): void {
-    if (index > this.activeIndex || (index === 0 && this.activeIndex === this.items.length - 1)) {
-      this.prevIndex = this.activeIndex;
-      this.activeIndex = index;
-      this.nextIndex = index < this.items.length - 1 ? index + 1 : 0;
-    } else if (index < this.activeIndex || (index === this.items.length - 1 && this.activeIndex === 0)) {
-      this.prevIndex = index;
-      this.activeIndex = index;
-      this.nextIndex = this.activeIndex < this.items.length - 1 ? this.activeIndex + 1 : 0;
-    }
+    this.activeIndex = index;
   }
 
   next(): void {
-    this.prevIndex = this.activeIndex;
-    this.activeIndex = this.nextIndex;
-    this.nextIndex = this.nextIndex < this.items.length - 1 ? this.nextIndex + 1 : 0;
-    if (this.activeIndex === 0) {
-      this.prevIndex = this.items.length - 1;
-    }
+    this.activeIndex = (this.activeIndex + 1) % this.items.length;
   }
 
   prev(): void {
-    this.nextIndex = this.activeIndex;
-    this.activeIndex = this.prevIndex;
-    this.prevIndex = this.prevIndex > 0 ? this.prevIndex - 1 : this.items.length - 1;
-    if (this.activeIndex === this.items.length - 1) {
-      this.nextIndex = 0;
-    }
+    this.activeIndex = (this.activeIndex - 1 + this.items.length) % this.items.length;
   }
 }
